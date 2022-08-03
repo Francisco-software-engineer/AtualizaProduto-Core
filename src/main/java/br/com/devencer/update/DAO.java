5eea19e0d31501c86b8400e1e2e0c731acd14fc7
Francisco Gonçalves <francisco.software.developer@gmail.com>
2022-08-03 16:37:41 -0300

    start-boilerplate

---
+++ src/main/java/br/com/devencer/update/DAO.java
@@ -0,0 +1,13 @@
+package br.com.devencer.update;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
+
+public class DAO {
+  List<Product> products = Arrays.asList(new Product("Feijao JJ", BigDecimal.valueOf(13.3)));
+
+
+
+  
+}
--- src/main/java/br/com/devencer/update/Product.java
+++ src/main/java/br/com/devencer/update/Product.java
@@ -1,5 +1,29 @@
 package br.com.devencer.update;
 
-public class Product {
+import java.math.BigDecimal;
 
+public class Product {
+  private String description;
+  private BigDecimal price;
+
+  public Product(String description, BigDecimal price) {
+    this.description = description;
+    this.price = price;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public BigDecimal getPrice() {
+    return price;
+  }
+
+  public void setPrice(BigDecimal price) {
+    this.price = price;
+  }
 }
