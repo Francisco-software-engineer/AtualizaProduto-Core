b270d99e3722d42fd3bc7f7f158aabbf0063dabd
Francisco Gonçalves <francisco.software.developer@gmail.com>
2022-08-03 16:37:41 -0300

    start-boilerplate

--- src/main/java/br/com/devencer/update/ProductDTO.java
+++ src/main/java/br/com/devencer/update/ProductDTO.java
@@ -1,5 +1,30 @@
 package br.com.devencer.update;
 
-public class ProductDTO {
+import java.math.BigDecimal;
 
+public class ProductDTO {
+  private String source;
+  private String description;
+  private BigDecimal price;
+
+  public ProductDTO(String description, BigDecimal price) {
+    this.description = description;
+    this.price = price;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public BigDecimal getPrice() {
+    return price;
+  }
+
+  public void setPrice(BigDecimal price) {
+    this.price = price;
+  }
 }
