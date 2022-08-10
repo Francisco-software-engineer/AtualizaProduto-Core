b66a9f8e59ebcac62d3f0a7aa76bbefb9abf2266
Francisco Gonçalves <francisco.software.developer@gmail.com>
2022-08-10 17:35:05 -0300

    test-filterupdate-updatelist-refactor

--- src/main/java/br/com/devencer/update/core/domain/entity/Product.java
+++ src/main/java/br/com/devencer/update/core/domain/entity/Product.java
@@ -1,25 +1,22 @@
 package br.com.devencer.update.core.domain.entity;
 
+import br.com.devencer.update.core.domain.rules.Product_Rules;
+import java.io.Serializable;
 import java.math.BigDecimal;
-import java.util.Collection;
-import java.util.Objects;
 
-public class Product implements Comparable<Product> {
+public class Product implements Comparable<Product>, Serializable {
   private final int id;
-
   private final String barcode;
   private String description;
   private BigDecimal price;
+  private Rule rules;
 
   public Product(int id, String barcode, String description, BigDecimal price) {
+    rules = Product_Rules.DEFAULT.getRule();
     this.id = id;
     this.barcode = barcode;
     this.description = description;
     this.price = price;
-  }
-
-  public String getDescription() {
-    return description;
   }
 
   public int getId() {
@@ -28,6 +25,10 @@ public class Product implements Comparable<Product> {
 
   public String getBarcode() {
     return barcode;
+  }
+
+  public String getDescription() {
+    return description;
   }
 
   public void setDescription(String description) {
@@ -42,50 +43,26 @@ public class Product implements Comparable<Product> {
     this.price = price;
   }
 
+  public Rule getRules() {
+    return rules;
+  }
+
+  public void setRules(Rule rules) {
+    this.rules = rules;
+  }
+
   @Override
   public int compareTo(Product anotherProduct) {
-    if (anotherProduct.toString() == ""){
-      return -1;
-    }
-    //if this == anotherProduct return 0
-    //if this > anotherProduct return 1
-    //if this < anotherProduct return -1
-    int compareResult = barcode.compareTo(anotherProduct.getBarcode());
-
-    if (compareResult == 0) {
-      return this.price.compareTo(anotherProduct.getPrice());
-    }else{
-      return compareResult;
-    }
-  }
-  @Override
-  public String toString() {
-    return barcode+price;
+    return rules.compareTo(this, anotherProduct);
   }
 
   @Override
   public boolean equals(Object o) {
-    if (this == o) {
-      return true;
-    }
-    if (o == null || getClass() != o.getClass()) {
-      return false;
-    }
-
-    Product product = (Product) o;
-
-    if (!Objects.equals(barcode, product.getBarcode())) {
-      return false;
-    }
-    return Objects.equals(price, product.getPrice());
+    return rules.equals(this, (Product)o);
   }
-
-
 
   @Override
   public int hashCode() {
-    int result = barcode != null ? barcode.hashCode() : 0;
-    result = 31 * result + (price != null ? price.hashCode() : 0);
-    return result;
+    return rules.hashCode(this);
   }
 }
