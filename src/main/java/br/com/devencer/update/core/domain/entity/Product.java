450f35ecf20d35fe0f07bcef0e75753cecfb3a1f
Francisco-software-engineer <110485446+Francisco-software-engineer@users.noreply.github.com>
2022-08-13 16:11:32 -0300

    Update Product.java

--- src/main/java/br/com/devencer/update/core/domain/entity/Product.java
+++ src/main/java/br/com/devencer/update/core/domain/entity/Product.java
@@ -4,25 +4,15 @@ import br.com.devencer.update.core.domain.rules.Product_Rules;
 import java.io.Serializable;
 import java.math.BigDecimal;
 
-<<<<<<< HEAD
-public class Product {
-  private int id;
-
-  private String barcode;
-=======
 public class Product implements Comparable<Product>, Serializable {
   private final int id;
   private final String barcode;
->>>>>>> feature/filter-updatelist
   private String description;
   private BigDecimal price;
   private Rule rules;
 
   public Product(int id, String barcode, String description, BigDecimal price) {
-<<<<<<< HEAD
-=======
     rules = Product_Rules.DEFAULT.getRule();
->>>>>>> feature/filter-updatelist
     this.id = id;
     this.barcode = barcode;
     this.description = description;
