c4e858a5a468572bf2136c52a9f53d39897a2017
Francisco Gonçalves <francisco.software.developer@gmail.com>
2022-08-15 11:51:22 -0300

    feature/driven

--- src/main/java/br/com/devencer/update/entity/Rule.java
+++
@@ -1,8 +0,0 @@
-package br.com.devencer.update.entity;
-
-public interface Rule {
-  int compareTo(Product product,Product anotherProduct);
-  String toString(Product product);
-  boolean equals(Product product,Product anotherProduct);
-  int hashCode(Product product);
-}
